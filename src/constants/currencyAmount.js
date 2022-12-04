bce3cc3ee5d0332762544f764ad2fb974c26cf6f
Anton689 <zizugum@gmail.com>
2022-12-04 22:50:24 +0200

    dist

--- src/constants/currencyAmount.js
+++
@@ -1,2 +0,0 @@
-export const FROM = 'from'
-export const TO = 'to'
--- src/store/mutationTypes.js
+++
@@ -1,8 +0,0 @@
-export const SET_CURRENCIES = 'SET_CURRENCIES'
-export const SET_TO = 'SET_TO'
-export const SET_FROM = 'SET_FROM'
-export const SET_MIN_AMOUNT = 'SET_MIN_AMOUNT'
-export const SET_ESTIMATE_AMOUNT = 'SET_ESTIMATE_AMOUNT'
-export const SET_ERROR = 'SET_ERROR'
-export const SET_PAIR_ERROR = 'SET_PAIR_ERROR'
-export const SWAP_CURRENCIES = 'SWAP_CURRENCIES'
